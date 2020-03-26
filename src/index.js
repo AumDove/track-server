63ba31130d2b52129a95522832a5c29ce9115148
aumdove <aumdove13@gmail.com>
2020-03-26 17:04:39 -0500

    finished API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,36 @@
+require('./models/User');
+require('./models/Track');
+
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
+const trackRoutes = require('./routes/trackRoutes');
+const requireAuth = require('./middlewares/requireAuth');
 
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send('Hi there!');
+app.use(bodyParser.json());
+app.use(authRoutes);
+app.use(trackRoutes);
+
+const mongoUri =
+	'mongodb+srv://aumdove:arnotts@cluster0-tahuf.mongodb.net/test?retryWrites=true&w=majority';
+mongoose.connect(mongoUri, {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useUnifiedTopology: true
+});
+
+mongoose.connection.on('connected', () => {
+	console.log('👍🏼 Connected to mongo instance!! 👍🏼');
+});
+mongoose.connection.on('error', err => {
+	console.error('💥 Error connecting to mongo 💥', err);
+});
+
+app.get('/', requireAuth, (req, res) => {
+	res.send(`Your email: ${req.user.email}`);
 });
 
 app.listen(3000, () => {
